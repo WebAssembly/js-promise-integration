a8d4bb4a8b8dd3e3fa24ad0a02069150d7a0403a
fgmccabe <fgmccabe@users.noreply.github.com>
2024-09-19 18:08:49 +0000

    deploy: 7506990193ea0a714b589663ca2a805fd4dff1c2

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 (Draft 2024-03-05)',
+    VERSION: '3.0 (Draft 2024-09-19)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
